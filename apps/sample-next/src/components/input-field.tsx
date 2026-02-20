b00f174095c92c7457c9fa6e97b54e00d2a9edc0
rubensantos <rubensantos994@hotmail.com>
2026-02-20 10:37:30 +0000

    feat(sample-next): show cursor not allowed when hovering in every disabled input

--- apps/sample-next/src/components/input-field.tsx
+++ apps/sample-next/src/components/input-field.tsx
@@ -24,7 +24,7 @@ export function InputField({
       <input
         id={id}
         type={type}
-        className="block w-full rounded-md border-0 py-1.5 bg-white dark:bg-neutral-800 text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-950 placeholder:text-gray-400 dark:placeholder:text-neutral-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        className="block w-full rounded-md border-0 py-1.5 bg-white dark:bg-neutral-800 text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-950 placeholder:text-gray-400 dark:placeholder:text-neutral-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pointer-events-auto disabled:cursor-not-allowed"
         placeholder={placeholder}
         value={value}
         onChange={onChange}
