6db49be75608a0ab07ce4cdbe6a1e4a17850be5d
rubensantos <rubensantos994@hotmail.com>
2026-03-11 16:38:36 +0000

    feat(sample-next): add a skeleton to user avatar while loading; replace js-sha256 with local implementation

--- apps/sample-next/src/components/gravatar.tsx
+++ apps/sample-next/src/components/gravatar.tsx
@@ -1,24 +1,47 @@
-import { sha256 } from "js-sha256";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { Skeleton } from "@/components/skeleton";
+import { logger } from "@/utils/logger";
+import { sha256 } from "@/utils/sha256";
 
 export function Gravatar({
   email,
   size = 128,
   fallbackUrl = "https://dashboard.notifica.re/assets/images/no-gravatar-blue.png",
 }: GravatarProps) {
-  const hash = sha256(email.trim().toLowerCase());
+  const [hash, setHash] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function loadHash() {
+      setLoading(true);
+
+      try {
+        const result = await sha256(email.trim().toLowerCase());
+        setHash(result);
+        console.log(result);
+      } catch (e) {
+        logger.error(`(User avatar) It was not possible to hash the email: ${e}`);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadHash();
+  }, [email]);
+
   const url = hash
     ? `https://www.gravatar.com/avatar/${hash}?s=${size}&d=${fallbackUrl}`
     : fallbackUrl;
 
   return (
-    <Image
-      className="h-8 w-8 rounded-full bg-gray-50"
-      src={url}
-      alt="Avatar"
-      width={32}
-      height={32}
-    />
+    <div className="relative">
+      {!loading ? (
+        <Image className="rounded-full shrink-0" src={url} alt="Avatar" width={32} height={32} />
+      ) : (
+        <Skeleton className="h-8 w-8 rounded-full bg-neutral-200 dark:bg-neutral-600" />
+      )}
+    </div>
   );
 }
 
