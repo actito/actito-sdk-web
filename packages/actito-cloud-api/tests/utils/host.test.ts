17bdadb09a0dab1288ea78c84ba5f5140303c7a0
rubensantos <rubensantos994@hotmail.com>
2026-05-08 17:39:17 +0100

    test(cloud-api): add unit tests

---
+++ packages/actito-cloud-api/tests/utils/host.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, test } from '@jest/globals';
+import { ensureHostHttpPrefix } from '~/utils';
+
+describe('test ensureHostHttpPrefix', () => {
+  test("when the host starts with 'http://', it returns the original host", () => {
+    const input = 'http://my-domain.com';
+    const expectedOutput = 'http://my-domain.com';
+
+    expect(ensureHostHttpPrefix(input)).toBe(expectedOutput);
+  });
+
+  test("when the host starts with 'https://', it returns the original host", () => {
+    const input = 'https://my-domain.com';
+    const expectedOutput = 'https://my-domain.com';
+
+    expect(ensureHostHttpPrefix(input)).toBe(expectedOutput);
+  });
+
+  test("when the host does not start with 'http://' or 'https://', it adds the 'https://' prefix to the host", () => {
+    const input = 'my-domain.com';
+    const expectedOutput = 'https://my-domain.com';
+
+    expect(ensureHostHttpPrefix(input)).toBe(expectedOutput);
+  });
+});
