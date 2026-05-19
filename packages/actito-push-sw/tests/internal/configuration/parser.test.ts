6e25a8788aa5e239097fdcd05af88688b178ffed
rubensantos <rubensantos994@hotmail.com>
2026-05-19 18:15:22 +0100

    test(push-sw): improve parseWorkerConfiguration tests structure and messages

--- packages/actito-push-sw/tests/internal/configuration/parser.test.ts
+++ packages/actito-push-sw/tests/internal/configuration/parser.test.ts
@@ -1,8 +1,22 @@
 import { describe, expect, jest, test, beforeEach } from '@jest/globals';
+import { parseWorkerConfiguration } from '~/internal/configuration/parser';
 import type { WorkerConfiguration } from '~/internal/configuration/worker-configuration';
 import type { WorkerNotification } from '~/internal/internal-types';
 
 describe('test parseWorkerConfiguration', () => {
+  // MOCKS
+  const mockServiceWorkerLocationSearch = (config: string) =>
+    Object.defineProperty(globalThis, 'self', {
+      value: {
+        ...globalThis.self,
+        location: {
+          ...globalThis.self.location,
+          search: config,
+        },
+      },
+    });
+
+  // DEFAULT DATA
   const FULL_WORKER_CONFIGURATION: WorkerConfiguration = {
     cloudHost: 'https://cloud.notifica.re',
     applicationId: 'app-123',
@@ -12,29 +26,14 @@ describe('test parseWorkerConfiguration', () => {
     standalone: true,
   };
 
-  const mockWorkerModule = (mockGetServiceWorkerLocation: () => Partial<WorkerLocation>) => {
-    jest.unstable_mockModule('~/internal/utils/worker', () => {
-      return {
-        getServiceWorkerLocation: () => mockGetServiceWorkerLocation(),
-        base64Decode: (data: string) => atob(data),
-      };
-    });
-  };
-
   beforeEach(() => {
     jest.resetModules();
   });
 
   test('when the worker has a valid configuration, it should return the configuration as expected', async () => {
-    const mockGetServiceWorkerLocation = () => {
-      const encodedConfig = btoa(JSON.stringify(FULL_WORKER_CONFIGURATION));
-
-      return { search: `?notificareConfig=${encodedConfig}` };
-    };
-
-    mockWorkerModule(mockGetServiceWorkerLocation);
-
-    const { parseWorkerConfiguration } = await import('~/internal/configuration/parser');
+    mockServiceWorkerLocationSearch(
+      `?notificareConfig=${encodeURIComponent(btoa(JSON.stringify(FULL_WORKER_CONFIGURATION)))}`,
+    );
 
     const expectedOutput: WorkerNotification = FULL_WORKER_CONFIGURATION;
 
@@ -42,34 +41,22 @@ describe('test parseWorkerConfiguration', () => {
   });
 
   test('when the worker configuration does not exist (missing notificareConfig search param), it should return undefined', async () => {
-    const mockGetServiceWorkerLocation = () => ({
-      search: '?someParam=123',
-    });
-
-    mockWorkerModule(mockGetServiceWorkerLocation);
-
-    const { parseWorkerConfiguration } = await import('~/internal/configuration/parser');
+    mockServiceWorkerLocationSearch('?someParam=123');
 
     expect(parseWorkerConfiguration()).toBeUndefined();
   });
 
   test.each([
-    ['cloudHost', { cloudHost: undefined }],
-    ['applicationKey', { applicationKey: undefined }],
-    ['applicationSecret', { applicationSecret: undefined }],
-    ['deviceId', { deviceId: undefined }],
+    ['no cloudHost', { cloudHost: undefined }],
+    ['no applicationKey', { applicationKey: undefined }],
+    ['no applicationSecret', { applicationSecret: undefined }],
+    ['no deviceId', { deviceId: undefined }],
   ])(
-    "when the worker configuration is missing the '%s' field, it should return undefined",
-    async (_, config: Partial<WorkerConfiguration>) => {
-      const mockGetServiceWorkerLocation = () => {
-        const encodedConfig = btoa(JSON.stringify({ ...FULL_WORKER_CONFIGURATION, ...config }));
-
-        return { search: `?notificareConfig=${encodedConfig}` };
-      };
-
-      mockWorkerModule(mockGetServiceWorkerLocation);
-
-      const { parseWorkerConfiguration } = await import('~/internal/configuration/parser');
+    'when the worker configuration is invalid (%s), it should return undefined',
+    async (_, configOverride: Partial<WorkerConfiguration>) => {
+      mockServiceWorkerLocationSearch(
+        `?notificareConfig=${encodeURIComponent(btoa(JSON.stringify({ ...FULL_WORKER_CONFIGURATION, ...configOverride })))}`,
+      );
 
       expect(parseWorkerConfiguration()).toBeUndefined();
     },
